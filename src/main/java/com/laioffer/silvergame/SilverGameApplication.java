b7b0b5a248f23ddd86689ebaebbf1dacadb1b6e9
Cheng <cai.cheng@northeastern.edu>
2025-04-26 17:37:54 -0400

    First commit for testing the git function.

---
+++ src/main/java/com/laioffer/silvergame/SilverGameApplication.java
@@ -0,0 +1,13 @@
+package com.laioffer.silvergame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilverGameApplication.class, args);
+    }
+
+}
