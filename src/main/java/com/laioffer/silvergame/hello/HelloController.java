3bed13e45e6a1cf4c14613d0291318761a8e2f64
Cheng <cai.cheng@northeastern.edu>
2025-04-27 10:42:47 -0400

    Updated build.gradle for new dependency as datafaker Updated HelloController.java for a new complete client-to-server test

--- src/main/java/com/laioffer/silvergame/hello/HelloController.java
+++ src/main/java/com/laioffer/silvergame/hello/HelloController.java
@@ -1,5 +1,5 @@
 package com.laioffer.silvergame.hello;
-
+import net.datafaker.Faker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +8,25 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-//        return "Hello World!";
-        return "Hi Allen. How is your weekend so far?";
+        Faker faker = new Faker();
+        String name = faker.name().fullName();
+        String company = faker.company().name();
+        String street = faker.address().streetAddress();
+        String city = faker.address().city();
+        String state = faker.address().state();
+        String bookTitle = faker.book().title();
+        String bookAuthor = faker.book().author();
+
+        String template = "This is %s. I work at %s. I live at %s in %s, %s. My favorite book is %s by %s";
+        return template.formatted(
+                name,
+                company,
+                street,
+                city,
+                state,
+                bookTitle,
+                bookAuthor
+        );
     }
 }
+
